1b5621a338a251c9f9f5781909f6b4ba2f2fd31d
junios-study <junios@naver.com>
2025-09-16 11:23:00 +0900

    화면 지우기
    화면 지우기

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 #include <windows.h>
 
 using namespace std;
@@ -18,6 +19,32 @@ int main()
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
 
 	cout << "P" << endl;
+	_getch();
+	system("cls");
+
+	PlayerX++;
+
+	//COORD Cur;
+	Cur.X = PlayerX;
+	Cur.Y = PlayerY;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+
+	cout << "P" << endl;
+	_getch();
+	system("cls");
+
+	PlayerY++;
+
+	//COORD Cur;
+	Cur.X = PlayerX;
+	Cur.Y = PlayerY;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+
+	cout << "P" << endl;
+	_getch();
+	system("cls");
+
+
 
 	return 0;
 }
