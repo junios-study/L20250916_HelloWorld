245481f37af6edf2bf11ff06376892381d76df4d
junios-study <junios@naver.com>
2025-09-16 11:01:09 +0900

    문자형 변수
    문자형 변수

--- main.cpp
+++ main.cpp
@@ -2,19 +2,16 @@
 
 using namespace std;
 
+//C++
+
 int main()
 {
-	//int, 산술 연산자.
-	int PlayerHP = 100;
-	int MonsterAttack = 10;
-	int PlayerDefence = 10;
-
-	int Damage = MonsterAttack - PlayerDefence;
-
-	PlayerHP = PlayerHP - Damage;
+	char C = 97;
+	char D = 'a';
 
 
-	cout << PlayerHP; //애니메이션 
+	cout << C << endl;
+	cout << D << endl;
 
 	return 0;
 }
